2e123aea4db3501f774fa4609e9fcf2934be52b3
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-07-02 11:05:16 +0200

    Refactored load DICOM file dialog

--- mosamaticdesktop/src/mosamaticdesktop/ui/dialogs/loaddicomfiledialog.py
+++ mosamaticdesktop/src/mosamaticdesktop/ui/dialogs/loaddicomfiledialog.py
@@ -1,72 +1,14 @@
-import os
-
-from PySide6.QtWidgets import (
-    QDialog,
-    QTextEdit,
-    QPushButton,
-    QFileDialog,
-    QLabel,
-    QVBoxLayout,
-)
-
 import mosamaticdesktop.ui.constants as constants
 
 from mosamaticdesktop.core.loaders.dicomfileloader import DicomFileLoader
 from mosamaticdesktop.core.data.datamanager import DataManager
-from mosamaticdesktop.ui.dialogs.dialog import Dialog
-from mosamaticdesktop.ui.settings import Settings
+from mosamaticdesktop.ui.dialogs.loadfiledialog import LoadFileDialog
 
 
-class LoadDicomFileDialog(Dialog):
+class LoadDicomFileDialog(LoadFileDialog):
     def __init__(self, parent=None):
         super(LoadDicomFileDialog, self).__init__(parent)
-        self._file_path_label = None
-        self._file_path_text_edit = None
-        self._open_file_select_dialog_button = None
-        self._load_button = None
-        self.init_layout()
-
-    def file_path_label(self):
-        if not self._file_path_label:
-            self._file_path_label = QLabel(constants.MOSAMATICDESKTOP_LOAD_DICOM_FILE_DIALOG_FILE_PATH_LABEL_TEXT)
-        return self._file_path_label
-    
-    def file_path_text_edit(self):
-        if not self._file_path_text_edit:
-            self._file_path_text_edit = QTextEdit(placeholderText=constants.MOSAMATICDESKTOP_LOAD_DICOM_FILE_DIALOG_FILE_PATH_TEXT_EDIT_PLACEHOLDER_TEXT)
-        return self._file_path_text_edit
-    
-    def open_file_select_dialog_button(self):
-        if not self._open_file_select_dialog_button:
-            self._open_file_select_dialog_button = QPushButton(constants.MOSAMATICDESKTOP_LOAD_DICOM_FILE_DIALOG_OPEN_SELECT_FILE_DIALOG_BUTTON_TEXT)
-            self._open_file_select_dialog_button.clicked.connect(self.handle_open_file_select_dialog_button)
-        return self._open_file_select_dialog_button
-    
-    def load_button(self):
-        if not self._load_button:
-            self._load_button = QPushButton(constants.MOSAMATICDESKTOP_LOAD_DICOM_FILE_DIALOG_LOAD_BUTTON_TEXT)
-            self._load_button.clicked.connect(self.handle_load_button)
-            self._load_button.setEnabled(False)
-        return self._load_button
-
-    def init_layout(self):
         self.setWindowTitle(constants.MOSAMATICDESKTOP_LOAD_DICOM_FILE_DIALOG_WINDOW_TITLE)
-        layout = QVBoxLayout()
-        layout.addWidget(self.file_path_label())
-        layout.addWidget(self.file_path_text_edit())
-        layout.addWidget(self.open_file_select_dialog_button())
-        layout.addWidget(self.load_button())
-        self.setLayout(layout)
-
-    def handle_open_file_select_dialog_button(self):
-        self.load_button().setEnabled(False)
-        last_directory = Settings().get(constants.MOSAMATICDESKTOP_LAST_DIRECTORY_KEY)
-        file_path, _ = QFileDialog.getOpenFileName(self, constants.MOSAMATICDESKTOP_LOAD_DICOM_FILE_DIALOG_OPEN_SELECT_FILE_PATH_DIALOG_WINDOW_TITLE, dir=last_directory)
-        if file_path:
-            self.file_path_text_edit().setText(file_path)
-            self.load_button().setEnabled(True)
-            directory = os.path.dirname(file_path)
-            Settings().set(constants.MOSAMATICDESKTOP_LAST_DIRECTORY_KEY, directory)
 
     def handle_load_button(self):
         loader = DicomFileLoader()
@@ -77,6 +19,3 @@ class LoadDicomFileDialog(Dialog):
             manager.add(data)
             print(f'Added data "{data.name()}" to data manager')
         self.close()
-
-    def clear(self):
-        self.file_path_text_edit().setText('')
