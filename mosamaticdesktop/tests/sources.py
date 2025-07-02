436f99192be1e1e8a669e555584801203f0262d7
Ralph Brecheisen <ralph.brecheisen@gmail.com>
2025-07-02 09:18:57 +0200

    Updated paths to model files dir

--- mosamaticdesktop/tests/sources.py
+++ mosamaticdesktop/tests/sources.py
@@ -4,16 +4,16 @@ SOURCES = {
     'mac': {
         'input': '/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/Mosamatic/testdata/L3',
         'model_files': {
-            'pytorch': '/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/Mosamatic/models/pytorch/2.2/L3',
-            'tensorflow': '/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/Mosamatic/models/tensorflow/1.0/L3',
+            'pytorch': '/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/Mosamatic/models/pytorch/L3/2.2',
+            'tensorflow': '/Users/ralph/Library/CloudStorage/GoogleDrive-ralph.brecheisen@gmail.com/My Drive/data/Mosamatic/models/tensorflow/L3/1.0',
         },
         'output': '/Users/ralph/Desktop/downloads/Mosamatic/CLI/output',
     },
     'windows': {
         'input': 'G:\\My Drive\\data\\Mosamatic\\testdata\\L3',
         'model_files': {
-            'pytorch': 'G:\\My Drive\\data\\Mosamatic\\models\\pytorch\\2.2\\L3',
-            'tensorflow': 'G:\\My Drive\\data\\Mosamatic\\models\\tensorflow\\1.0\\L3',
+            'pytorch': 'G:\\My Drive\\data\\Mosamatic\\models\\pytorch\\L3\\2.2',
+            'tensorflow': 'G:\\My Drive\\data\\Mosamatic\\models\\tensorflow\\L3\\1.0',
         },
         'output': 'D:\\Mosamatic\\CLI\\Output',
     }
