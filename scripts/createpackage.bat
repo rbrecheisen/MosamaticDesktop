9be44c1dc055f4cbf215685301f33a8eb50e593d
Brecheisen <r.brecheisen@maastrichtuniversity.nl>
2025-07-18 10:44:55 +0200

    Finished installation packaging script

--- scripts/createpackage.bat
+++ scripts/createpackage.bat
@@ -4,19 +4,20 @@ setlocal
 
 echo Create output directory (delete if already exists)...
 set OUTPUT_DIR=D:\Mosamatic\MosamaticDesktop
+set OUTPUT_ZIP=D:\Mosamatic\MosamaticDesktop.zip
 rmdir /s /q %OUTPUT_DIR%
 mkdir %OUTPUT_DIR% 2>nul
 
 robocopy mosamaticdesktop %OUTPUT_DIR% /E /XD __pycache__ *.dist-info .pytest_cache build dist logs /XF .gitignore CHANGELOG
 
 copy requirements.txt %OUTPUT_DIR%
-copy scripts\clean-python-environment.bat %OUTPUT_DIR%
-copy scripts\clean-python-environment.ps1 %OUTPUT_DIR%
-copy scripts\run-mosamaticdesktop.bat %OUTPUT_DIR%
+copy scripts\packagefiles\clean-python-environment.bat %OUTPUT_DIR%
+copy scripts\packagefiles\clean-python-environment.ps1 %OUTPUT_DIR%
+copy scripts\packagefiles\run-mosamaticdesktop.bat %OUTPUT_DIR%
 
-powershell -Command "Compress-Archive -Path '%OUTPUT_DIR%' -DestinationPath '%OUTPUT_DIR%\..\MosamaticDesktop.zip' -Force"
+powershell -Command "Compress-Archive -Path '%OUTPUT_DIR%' -DestinationPath '%OUTPUT_ZIP%' -Force"
 
-echo Created ZIP archive: %OUTPUT_DIR%\..\MosamaticDesktop.zip
+echo Created ZIP archive: %OUTPUT_ZIP%
 echo Finished
 
 endlocal
